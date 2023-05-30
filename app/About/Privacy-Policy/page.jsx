4ef92243f0421e025d034d43780317ac6f3344e3
Piyush <piyyush1219@gmail.com>
2023-05-30 19:55:27 +0530

    PrivacyPolicyPage

---
+++ app/About/Privacy-Policy/page.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className=" w-full md:w-[90%] px-5 py-10 md:p-10 mt-[85px]">
+      <div className="mb-10">
+        <h1 className=" text-4xl md:text-5xl font-bold">Privacy Policy</h1>
+        <p className="text-lg text-gray-500">
+          <b>Last Modified:</b>May 25,2022
+        </p>
+      </div>
+      <p className=" text-sm md:text-lg text-gray-500 py-10">
+        Kellton and its affiliates are committed to protecting your privacy.
+        This Privacy Policy applies to our website (www.kellton.com) and governs
+        our data collection, processing, and usage practices. It also describes
+        your choices on the use, access, and correction of your personal
+        information. If you do not agree with the data practices described in
+        this Privacy Policy, you are not entitled to use the Websites or the
+        Subscription Service.We update this Privacy Policy regularly. All policy
+        updates are posted on this page and if the changes are significant, we
+        will intimate you through an email. While we notify you of any
+        significant changes to this Privacy Policy, we encourage you to review
+        this Privacy Policy periodically. We will also keep older versions of
+        this Privacy Policy in an archive for your review.If you have any
+        questions about this Privacy Policy or our handling of the information
+        you provide us, please write to us at privacy@kellton.com or Kellton,
+        1367, Road No. 45, Nandagiri Hills, Jubilee Hills, Hyderabad, Telangana
+        500033
+      </p>
+      <h1 className="text-2xl py-10">Use of the Subscription Service</h1>
+      <p className=" text-sm md:text-lg text-gray-500 py-10">
+        Our online Subscription Service allows us to create and share marketing mailers and collaterals. The information added to the Subscription Service by users while subscribing to the website is stored and managed by our servers. This information is then used to contact visitors about their interest in the company's services. Kellton provides the Subscription Service to our clients for their own marketing, lead generation, and customer service needs, and we use the same for our business requirements. We use our own Subscription Service to build web pages that people can visit to learn more about Kellton. Information that we collect and manage using the Subscription Service belongs to us and is used, disclosed, and protected according to this Privacy Policy.
+      </p>
+      <h1 className="text-2xl py-10">How We Use Information We Collect</h1>
+      <p className=" text-sm md:text-lg text-gray-500 py-10">
+        Our online Subscription Service allows us to create and share marketing mailers and collaterals. The information added to the Subscription Service by users while subscribing to the website is stored and managed by our servers. This information is then used to contact visitors about their interest in the company's services. Kellton provides the Subscription Service to our clients for their own marketing, lead generation, and customer service needs, and we use the same for our business requirements. We use our own Subscription Service to build web pages that people can visit to learn more about Kellton. Information that we collect and manage using the Subscription Service belongs to us and is used, disclosed, and protected according to this Privacy Policy.
+      </p>
+    </div>
+  );
+};
+
+export default page;
