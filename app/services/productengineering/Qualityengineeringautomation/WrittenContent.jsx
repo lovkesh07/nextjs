e713be684a6d0bdc5fb47f8ee580f3cf5207c54a
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2023-06-01 20:20:41 +0530

    1 new folder completed

---
+++ app/services/productengineering/Qualityengineeringautomation/WrittenContent.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const WrittenContent = () => {
+  return (
+    <div className="w-full p-10 md:p-16">
+      <h1 className=" text-xl sm:text-4xl md:text-5xl">
+        We use cutting-edge quality engineering expertise to maximize your
+        product’s potential.
+      </h1>
+      <div className="py-10 w-full sm:w-[85%]">
+        <p className=" sm:ml-16 text-sm sm:text-xl">
+          For digital success, constant testing and analysis is everything. With
+          competition fierce, it only takes one performance issue for your
+          customers to uninstall your app and find an equivalent elsewhere. The
+          right balance of automated and manual quality analysis (QA) workflows
+          optimizes app performance and data processing, whilst allowing you to
+          fix potential issues before they affect your user base.
+          <br />
+          <br />
+          <br />
+          Our quality engineering teams measure the maturity of your existing
+          processes, define the best-suited QA, testing, support, and
+          maintenance solutions, and facilitate agility to adapt to shifting
+          markets. This allows you to maximize the value of your products by
+          ensuring their speed, quality, and productivity.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default WrittenContent;
