28c592fb48b38be9a35a14ce4bf24063cb2d875d
Piyush <piyyush1219@gmail.com>
2023-06-12 23:21:07 +0530

    writeenContentUpdated

--- app/services/ITTrainingandEducation/page.jsx
+++ app/services/ITTrainingandEducation/page.jsx
@@ -1,11 +1,23 @@
+"use client";
 import React from "react";
 import Slide1 from "@assets/img1.jpg";
 import Image from "next/image";
 // import Sponsor from "./Sponsor";
 // import Sectionextra from "./sectionexra";
-import WrittenContent from "./WrittenContent";
+import WrittenContent from "../../WrittenContent";
+import { useState, useEffect } from "react";
+
 
 const AboutUs = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    setTitle("IT Training and Education:");
+
+    setContent(
+      "Staying up-to-date with the latest technology trends and acquiring relevant skills is essential in the fast-paced IT landscape. Our training and education programs provide comprehensive learning opportunities for individuals and organizations. We offer workshops, seminars, and customized training sessions in areas such as software development, cybersecurity, cloud computing, and data analytics. Our experienced trainers deliver hands-on training, equipping you and your team with the knowledge and skills needed to excel in the ever-evolving IT industry."
+    );
+  }, []);
   return (
     <div className="">
       <div className="relative">
@@ -19,12 +31,11 @@ const AboutUs = () => {
         <div className=" absolute top-[50%] translate-y-[-50%] text-white px-10 md:px-16">
           <p className="text-lg">Home/ IT Training and Education</p>
           <h1 className="text-3xl sm:text-5xl font-bold">
-          IT Training and Education
+            IT Training and Education
           </h1>
-          
         </div>
       </div>
-      <WrittenContent />
+      <WrittenContent title={title} content={content} />
     </div>
   );
 };
--- app/services/ITsupport&managedservices/page.jsx
+++ app/services/ITsupport&managedservices/page.jsx
@@ -1,11 +1,22 @@
+"use client";
 import React from "react";
 import Slide1 from "@assets/img1.jpg";
 import Image from "next/image";
 // import Sponsor from "./Sponsor";
 // import Sectionextra from "./sectionexra";
-import WrittenContent from "./WrittenContent";
+import WrittenContent from "../../WrittenContent";
+import { useState, useEffect } from "react";
 
 const page = () => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  useEffect(() => {
+    setTitle("IT Support and Managed Services:");
+
+    setContent(
+      "Count on our dedicated support team for prompt assistance and proactive monitoring of your IT infrastructure. We handle troubleshooting, hardware and software issues, as well as managing backups and updates. Our comprehensive support services are tailored to meet your specific needs."
+    );
+  }, []);
   return (
     <div className="">
       <div className="relative">
@@ -17,11 +28,15 @@ const page = () => {
           className=" w-full h-[80vh]"
         />
         <div className=" absolute top-[50%] translate-y-[-50%] text-white px-10 md:px-16">
-          <p className="text-lg">Home/ Services/ IT Support & Managed Services </p>
-          <h1 className="text-3xl sm:text-5xl font-bold">IT Support & Managed Services</h1>
+          <p className="text-lg">
+            Home/ Services/ IT Support & Managed Services{" "}
+          </p>
+          <h1 className="text-3xl sm:text-5xl font-bold">
+            IT Support & Managed Services
+          </h1>
         </div>
       </div>
-      <WrittenContent />
+      <WrittenContent title={title} content={content} />
     </div>
   );
 };
