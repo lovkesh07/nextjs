6cd6f93863ed4e557c5d8280750d27c042004ae7
Piyush <piyyush1219@gmail.com>
2023-06-13 21:49:38 +0530

    main-page

--- components/Card.jsx
+++ components/Card.jsx
@@ -12,7 +12,7 @@ const Cards = () => {
             Our Services
           </p>
           <p
-            className=" w-[80%] font-bold text-4xl md:text-5xl pb-4"
+            className=" w-[80%] font-bold text-2xl sm:text-4xl md:text-5xl pb-4"
             data-aos="fade-up"
           >
             We offer a comprehensive range of advanced IT solutions and services
@@ -22,13 +22,13 @@ const Cards = () => {
         </div>
         <div className="py-16 lg:py-16 w-[85%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
           <div
-            className=" w-full md:w-[100%] h-[40vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full md:w-[100%] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/itconsultingservices")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               IT Consulting Services:
             </h2>
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Our expert consultants conduct in-depth assessments of your
               existing IT infrastructure, aligning them with your business
               objectives. Through our customized recommendations, we optimize
@@ -41,13 +41,13 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full md:w-[100%] h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full md:w-[100%] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/customsoftwaredevelopment")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               Custom Software Development:
             </h2>
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               With our cutting-edge software development expertise, we create
               tailored solutions that cater to your unique business
               requirements. Our skilled developers leverage the latest
@@ -61,14 +61,14 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full  h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/cloudcomputingsolutions")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               Cloud Computing Solutions:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Experience the power of cloud technology through our comprehensive
               range of services. We facilitate seamless migration of your IT
               infrastructure to the cloud and develop cloud-native applications.
@@ -82,14 +82,14 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/cybersecurityservices")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               Cybersecurity Services:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Safeguarding your digital assets is our top priority. Our
               cybersecurity solutions include thorough threat assessments,
               vulnerability scanning, and penetration testing. We implement
@@ -103,14 +103,14 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-blue-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/ITsupport&managedservices")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               IT Support and Managed Services:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Count on our dedicated support team for prompt assistance and
               proactive monitoring of your IT infrastructure. We handle
               troubleshooting, hardware and software issues, as well as managing
@@ -123,14 +123,14 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/DataAnalyticsandBusinessIntelligence")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               Data Analytics and Business Intelligence:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Unlock valuable insights from your data to make informed
               decisions. Our data analytics and business intelligence solutions
               employ advanced tools and techniques to transform raw data into
@@ -143,14 +143,14 @@ const Cards = () => {
           </div>
 
           <div
-            className=" w-full h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
+            className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100"
             data-aos="fade-up" onClick={() => router.push("/services/ITTrainingandEducation")}
           >
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               IT Training and Education:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-xl text-white px-3 py-2">
+            <p className=" text-xs sm:text-sm  md:text-base lg:text-lg text-white px-3 py-2">
               Stay ahead in the dynamic technology landscape with our training
               programs and workshops. We empower individuals and organizations
               with knowledge and skills in software development, cybersecurity,
@@ -162,12 +162,12 @@ const Cards = () => {
             </span>
           </div>
 
-          <div className=" w-full h-[55vh] md:h-[40vh] cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100" data-aos="fade-up" onClick={() => router.push("/services/ArVrservices")}>
+          <div className=" w-full cursor-pointer gap-3 md:gap-0 mx-auto flex flex-col items-center rounded-md shadow-md bg-purple-500 hover:shadow-black hover:scale-[1.01] transition-all duration-300 delay-100" data-aos="fade-up" onClick={() => router.push("/services/ArVrservices")}>
             <h2 className=" w-full text-left px-3 py-2 text-xl lg:text-2xl text-white">
               AR/VR Services:
             </h2>
 
-            <p className=" text-sm md:text-lg lg:text-sm text-white lg:px-8 px-3 py-2">
+            <p className=" text-xs sm:text-sm text-white lg:px-8 px-3 py-2">
               We offer cutting-edge Augmented Reality (AR) and Virtual Reality
               (VR) solutions to enhance user experiences and create immersive
               digital environments. Our skilled team combines innovative
