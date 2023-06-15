e1b1bc2bc43c9c59fd9c44260f71e3b0d0eff3cf
Piyush <piyyush1219@gmail.com>
2023-06-15 20:45:38 +0530

    content-parrt

--- app/Content.jsx
+++ app/Content.jsx
@@ -5,7 +5,7 @@ const Content = ({title,content}) => {
     <div className=' bg-[#1A374D] text-white min-w-full p-8'>
       <div className=' relative my-8 mx-auto w-full h-full'>
         <div className='absolute bg-[#1A374D] z-10 text-3xl font-bold top-[-20px] left-[-24px]'>{title}</div>
-        <div className=' border-[1px] py-10 px-5 sm:py-7 md:py-10 border-white border-solid h-full text-xs sm:text-sm md:text-base lg-text-lg'>{content}</div>
+        <div className=' border-[1px] py-10 px-5 sm:py-7 md:py-10 border-white border-solid h-full text-lg'>{content}</div>
       </div>
       
     </div>
