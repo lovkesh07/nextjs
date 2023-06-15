91cfb090e4695ff0f5ed31d0fcf559f11a3392bf
lovkesh07 <79805511+lovkesh07@users.noreply.github.com>
2023-06-15 22:21:34 +0530

    gg

--- app/Content.jsx
+++ app/Content.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Content = ({title,content}) => {
   return (
-    <div className=' bg-[#1A374D] text-white min-w-full p-16' >
+    <div className=' bg-[#1A374D] text-white min-w-full p-6 lg:p-16' >
       <div className=' relative my-8 mx-auto w-full h-full'>
         <div className='absolute bg-[#1A374D] z-10 text-3xl font-bold top-[-20px] left-[-24px]'>{title}</div>
         <div className=' border-[1px] py-10 px-5 sm:py-7 md:py-10 border-white border-solid h-full text-lg'  data-aos="fade-up">{content}</div>
