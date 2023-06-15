3a2bef219fbddc6e54f5333d9b9ad7a7725f4819
Piyush <piyyush1219@gmail.com>
2023-06-15 12:43:40 +0530

    nav-color

--- app/services/cloudcomputingsolutions/page.jsx
+++ app/services/cloudcomputingsolutions/page.jsx
@@ -23,7 +23,7 @@ const AboutUs = () => {
     setmaincontent("Our cloud computing services enable you to leverage the power of cloud technology for enhanced efficiency and flexibility. We help you migrate your IT infrastructure to the cloud, ensuring a smooth transition and optimizing resource allocation. Additionally, we develop cloud-native applications, taking full advantage of cloud capabilities. Our team assists with cloud strategy formulation, cloud infrastructure setup, data migration, and ongoing cloud management to ensure your business benefits from increased scalability, cost-efficiency, and accessibility.")
   }, []);
   return (
-    <div className="">
+    <div className="mt-[80px]">
       <div className="relative">
         <Image
           src={Slide1}
