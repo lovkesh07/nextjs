9f77986fc529715ad1e9bf28f641b2a5d13cfe6a
Piyush <piyyush1219@gmail.com>
2023-06-15 13:07:16 +0530

    customglitch

--- app/services/customsoftwaredevelopment/page.jsx
+++ app/services/customsoftwaredevelopment/page.jsx
@@ -18,8 +18,7 @@ const AboutUs = () => {
   useEffect(() => {
     setTitle("Custom Software Development");
 
-    setContent(
-      "With our cutting-edge software development expertise, we create tailored solutions that cater to your unique business requirements. Our skilled developers leverage the latest technologies to build secure, scalable, and user-friendly software, including web and mobile applications, as well as enterprise systems.");
+    setContent("With our cutting-edge software development expertise, we create tailored solutions that cater to your unique business requirements. Our skilled developers leverage the latest technologies to build secure, scalable, and user-friendly software, including web and mobile applications, as well as enterprise systems.");
     setmaincontent("We specialize in creating customized software applications to meet your specific business requirements. Our skilled development team utilizes the latest technologies, programming languages, and frameworks to design and build software solutions. Whether you need a web application, mobile app, or enterprise software system, we ensure that the software is scalable, secure, and user-friendly, providing seamless functionality and a superior user experience.")
   }, []);
   return (
