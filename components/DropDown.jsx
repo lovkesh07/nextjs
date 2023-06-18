6fb5fee75b0a1d04b958791a406a0664f7e17969
Piyush <piyyush1219@gmail.com>
2023-06-18 22:23:15 +0530

    nav

--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -8,8 +8,6 @@ const DropDown = ({ title, list1 }) => {
   const [IsOpen, setIsOpen] = useState(false);
   const divRef = useRef();
 
-
-
   useEffect(() => {
     window.addEventListener("click", (e) => {
       if (e.target !== divRef.current) {
@@ -19,7 +17,7 @@ const DropDown = ({ title, list1 }) => {
   });
 
   return (
-    <div className=" w-full">
+    <div className=" group w-full">
       <button
         ref={divRef}
         onClick={() => setIsOpen(!IsOpen)}
@@ -32,26 +30,23 @@ const DropDown = ({ title, list1 }) => {
       ) : (
         <ArrowDropDownIcon fontSize="small" />
       )}
-
-      {IsOpen && (
-        <div className=" text-sm z-50 w-screen py-5 px-5 grid grid-cols-3 h-fit bg-gray-100 rounded-md text-[#1A374D] absolute md:top-[85px] left-0  opacity-100  transition-all duration-300">
-          {list1.map((module, i) => {
-            return (
-              <div
-                key={i}
-                className="  flex flex-row-reverse items-center justify-center text-[0.9rem] w-full py-2 mx-auto hover:bg-gray-300 transition-all duration-300"
+      <div className={` top-0 group-hover:top-[80px] text-sm z-50 w-screen py-5 px-5 grid grid-cols-3 h-fit bg-gray-100 rounded-md text-[#1A374D] absolute md:top-[85px] left-0  opacity-100  transition-all duration-300`}>
+        {list1.map((module, i) => {
+          return (
+            <div
+              key={i}
+              className="  flex flex-row-reverse items-center justify-center text-[0.9rem] w-full py-2 mx-auto hover:bg-gray-300 transition-all duration-300"
+            >
+              <Link
+                className=" relative mx-auto flex flex-row-reverse items-center justify-center"
+                href={module.link}
               >
-                <Link
-                  className=" relative mx-auto flex flex-row-reverse items-center justify-center"
-                  href={module.link}
-                >
-                  {module.name}
-                </Link>
-              </div>
-            );
-          })}
-        </div>
-      )}
+                {module.name}
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
