5246d2e7103fd78c19571877e192c6979378b0c5
Piyush <piyyush1219@gmail.com>
2023-06-18 22:57:00 +0530

    onhovernav

--- components/DropDown.jsx
+++ components/DropDown.jsx
@@ -25,28 +25,25 @@ const DropDown = ({ title, list1 }) => {
       >
         {title}
       </button>
-      {IsOpen ? (
-        <ArrowDropUpIcon fontSize="small" />
-      ) : (
-        <ArrowDropDownIcon fontSize="small" />
-      )}
-      <div className={` top-0 group-hover:top-[80px] text-sm z-50 w-screen py-5 px-5 grid grid-cols-3 h-fit bg-gray-100 rounded-md text-[#1A374D] absolute md:top-[85px] left-0  opacity-100  transition-all duration-300`}>
-        {list1.map((module, i) => {
-          return (
-            <div
-              key={i}
-              className="  flex flex-row-reverse items-center justify-center text-[0.9rem] w-full py-2 mx-auto hover:bg-gray-300 transition-all duration-300"
-            >
-              <Link
-                className=" relative mx-auto flex flex-row-reverse items-center justify-center"
-                href={module.link}
+        <div
+          className=" invisible group-hover:visible top-[82px] text-sm z-50 w-screen py-5 px-5 grid grid-cols-3 h-fit bg-gray-100 rounded-md text-[#1A374D] absolute left-0"
+        >
+          {list1.map((module, i) => {
+            return (
+              <div
+                key={i}
+                className="  flex flex-row-reverse items-center justify-center text-[0.9rem] w-full py-2 mx-auto hover:bg-gray-300 transition-all duration-300"
               >
-                {module.name}
-              </Link>
-            </div>
-          );
-        })}
-      </div>
+                <Link
+                  className=" relative mx-auto flex flex-row-reverse items-center justify-center"
+                  href={module.link}
+                >
+                  {module.name}
+                </Link>
+              </div>
+            );
+          })}
+        </div>
     </div>
   );
 };
