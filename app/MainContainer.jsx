d4eebe545602d8832857929a14647b809e6e208a
Piyush <piyyush1219@gmail.com>
2023-06-19 16:26:42 +0530

    blur effect ablurdded

--- app/MainContainer.jsx
+++ app/MainContainer.jsx
@@ -12,13 +12,13 @@ const MainContainer = ({ serviceName, serviceContent, Img, serviceLink }) => {
       {/* Team section */}
 
       <div
-        className="mx-auto border-[1px] border-black border-solid flex flex-col items-center justify-center"
+        className=" group mx-auto border-[1px] border-black border-solid flex flex-col items-center justify-center"
         data-aos="fade-up"
       >
         <div className="mx-auto max-w-7xl">
           <div className="mx-auto">
             <div className=" h-[600px] w-full">
-              <div className=" h-[40%]">
+              <div className=" group-hover:backdrop-filter group-hover:backdrop-blur-md group-hover:opacity-50 group-hover:bg-white h-[40%] transition-all duration-200">
                 <Image
                   width={1000}
                   height={1000}
@@ -26,6 +26,10 @@ const MainContainer = ({ serviceName, serviceContent, Img, serviceLink }) => {
                   src={Img}
                   alt="a"
                 />
+                <Link href={serviceLink} className=" hidden px-5 py-3 group-hover:block  text-white bg-transparent absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] border-[1px] border-solid border-white rounded-md">
+                  {" "}
+                  Read more{" "}
+                </Link>
               </div>
               <div className=" p-5 h-[60%] bg-gray-100 w-full relative">
                 <h3 className="text-lg font-semibold text-center leading-8 tracking-tight text-black">
@@ -34,10 +38,10 @@ const MainContainer = ({ serviceName, serviceContent, Img, serviceLink }) => {
                 <p className="text-sm leading-6 text-gray-500 py-5">
                   {serviceContent}
                 </p>
-                <Link href={serviceLink} className=" py-5 text-black absolute bottom-[0px] right-[20px]">
+                {/* <Link href={serviceLink} className=" py-5 text-black absolute bottom-[0px] right-[20px]">
                   {" "}
                   Read more{" "}
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
