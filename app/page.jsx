26d5813aeb1f93302a3d3f46bec61f118679f316
Piyush <piyyush1219@gmail.com>
2023-06-19 10:54:26 +0530

    error

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Cards from "@components/Card";
 import Longcard from "@components/Longcard";
-<<<<<<< HEAD
 // import Sponser from "@components/Sponsor";
-// import ImgSlider from "@components/ImgSlider";
-=======
-import Sponser from "@components/Sponsor";
 import ImgSlider from "@components/ImgSlider";
->>>>>>> 25fa52696092898cebacade8f4f60b94daea4e1b
 import Ourmission from "@components/Ourmission";
 // import Testimonialpg from "@components/Testimonialpg";
 import AnimationSlider from "@components/AnimationSlider";
