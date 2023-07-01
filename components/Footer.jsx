e6d71af3e580b4662db8afd6dcb7199699b01b34
Piyush <piyyush1219@gmail.com>
2023-07-01 22:51:07 +0530

    done..

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -26,24 +26,23 @@ function Footer({ List1 }) {
           })}
         </div>
       </div>
-      <div className="flex flex-row justify-center items-center text-center  p-5 bg-black border-t-[1px] border-white">
-        <h1 className=" text-white text-sm mr-[2px]">
-          ©2023-2024
-        </h1>
-        <h1 className=" text-white text-sm hover:text-blue-500">
+      <div className="flex flex-row justify-center items-center text-center  py-5 bg-black">
+        <h1 className=" text-white text-xs mr-[2px]">©2023-2024</h1>
+        <h1 className=" text-white text-xs hover:text-blue-500">
           All rights reserved Dotclu{" "}
         </h1>
-        <div className=" h-[20px] w-[2px] mx-[12px] bg-white"></div>
+      </div>
+      <div className="flex flex-row justify-center items-center text-center  py-5 bg-black border-t-[1px] border-white">
         <Link
           href="/privacy&policy"
-          className=" cursor-pointer text-sm text-blue-500 border-b-[1px] border-blue-500"
+          className=" cursor-pointer text-xs text-white"
         >
           Privacy & Policy
         </Link>
         <div className=" h-[20px] w-[2px] mx-[12px] bg-white"></div>
         <Link
           href="/term&condition"
-          className=" cursor-pointer text-sm text-blue-500 border-b-[1px] border-blue-500"
+          className=" cursor-pointer text-xs text-white"
         >
           Term & Condition
         </Link>
